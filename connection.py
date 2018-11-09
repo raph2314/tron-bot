896a447a491b1427bb724b802216e982373ead81
freddessert <fred.des.16@hotmail.com>
2018-11-09 18:54:52 -0500

    Hopefully final connection

--- connection.py
+++ connection.py
@@ -4,22 +4,31 @@ import json
 
 websocket_url = 'ws://35.183.103.104:8080/connect_dev'
 
+team_id = '<YOUR TEAM ID>'
+team_key = '<YOUR KEY>'
+
 sample_request = {
     'type':'REGISTRATION',
     'message':'',
+    'authenticationKey':team_key,
+    'team_id':team_id
+    }
+
+sample_move = {
+    'type':'MOVE',
+    'message':'UP',
     'authenticationKey':'<YOUR KEY>',
     'team_id':'<YOUR TEAM ID>'
     }
 
 async def hello():
     async with websockets.connect(websocket_url) as websocket:
-        message = input(json.dumps(sample_request))
-        print(sample_request)
+        message = json.dumps(sample_request)
 
         await websocket.send(message)
-        print(f"> {message}")
 
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
+        answer = await websocket.recv()
+        
+        print(f"< FIRST_ANSWER :{answer}")
 
 asyncio.get_event_loop().run_until_complete(hello())
